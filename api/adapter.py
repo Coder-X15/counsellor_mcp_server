8a916425f4a2ccbac39fb1c966c8c2a766ea202f
Coder-X15 <samrubenabraham@gmail.com>
2025-06-22 16:43:05 +0530

    Commit 6

--- api/adapter.py
+++ api/adapter.py
@@ -8,17 +8,16 @@ def build_app(mcp: FastMCP) -> Flask:
 
     @app.route("/tools", methods=["POST", "GET"])
     def get_tools():
-        with Client(mcp) as client:
-            tools = client.list_tools()
-            return tools
-    
+        client = Client(mcp)
+        tools = client.list_tools()
+        return tools
+
     @app.route("/tools/<tool_name>", methods=["POST"])
     def run_tool(tool_name: str):
-        with Client(mcp) as client:
-            tool = client.get_tool(tool_name)
-            if tool:
-                return client.call_tool(tool_name, arguments = request.get_json())
-            
-            return "Tool not found", 404
-    
+        client = Client(mcp)
+        tool = client.get_tool(tool_name)
+        if tool:
+            return client.call_tool(tool_name, arguments=request.get_json())
+        return "Tool not found", 404
+
     return app
