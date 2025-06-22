49340273cf8db3003759412473b0c01d88e80958
Coder-X15 <samrubenabraham@gmail.com>
2025-06-22 16:46:48 +0530

    Commit 7

--- api/adapter.py
+++ api/adapter.py
@@ -1,5 +1,6 @@
+import asyncio
 from fastmcp import Client, FastMCP
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 # our aim is to provide a Flask-based API that can be used to interact with the FastMCP server
 
@@ -9,15 +10,16 @@ def build_app(mcp: FastMCP) -> Flask:
     @app.route("/tools", methods=["POST", "GET"])
     def get_tools():
         client = Client(mcp)
-        tools = client.list_tools()
-        return tools
+        tools = asyncio.run(client.list_tools())
+        return jsonify(tools)
 
     @app.route("/tools/<tool_name>", methods=["POST"])
     def run_tool(tool_name: str):
         client = Client(mcp)
-        tool = client.get_tool(tool_name)
+        tool = asyncio.run(client.get_tool(tool_name))
         if tool:
-            return client.call_tool(tool_name, arguments=request.get_json())
+            result = asyncio.run(client.call_tool(tool_name, arguments=request.get_json()))
+            return jsonify(result)
         return "Tool not found", 404
 
     return app
